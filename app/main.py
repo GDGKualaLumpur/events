014c0930c107cd2be70bee1cdbe8141628a768bc
Henry Lim <henrylim.everything@gmail.com>
2017-09-13 03:02:32 +0800

    update

--- app/main.py
+++ app/main.py
@@ -8,7 +8,21 @@ class Tfxkl17(webapp2.RequestHandler):
     def get(self, *args, **kwargs):
         self.redirect('//' + self.request.host + '/tfxkl17/')
 
+class TensorflowDevSummit(webapp2.RequestHandler):
+  def get(self, *args, **kwargs):
+    path = self.request.path_qs.split('/')
+    if (path[1] != 'tensorflow-dev-summit'):
+        self.redirect('https://www.gdgkl.org')
+    else:
+        self.redirect('//' + self.request.host + '/tfxkl17/' + '/'.join(path[2:]))
+
+class Jomcodekl17(webapp2.RequestHandler):
+    def get(self, *args, **kwargs):
+        self.redirect('//' + self.request.host + '/jomcodekl17/')
+
 app = webapp2.WSGIApplication([
     ('/tfxkl17.*', Tfxkl17),
+    ('/tensorflow-dev-summit.*', TensorflowDevSummit),
+    ('/jomcodekl17.*', Jomcodekl17),
     ('/.*', MainPage),
 ], debug=True)
